3099ba1b636954ce78a5e3ecc2541a1b7502cf4d
Satish Patil <satish.patil7017@gmail.com>
2022-09-22 15:15:11 +0530

    [Satish] Added Creating Address Book DataBase

---
+++ AddressBookADO.net/AddressBookADO.net/AddressBookRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADO.net
+{
+    public class AddressBookRepo
+    {
+        public void Create_Database()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=(localdb)\ProjectModels; Initial Catalog =master; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand command = new SqlCommand("Create database AddressbookForADO;", Connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("AddressbookService Database created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+    }
+}
---
+++ AddressBookADO.net/AddressBookADO.net/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AddressBookADO.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.NET!");
+            Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase");
+            int op = Convert.ToInt16(Console.ReadLine());
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+
+            switch (op)
+            {
+                case 1:
+                    addressBookRepo.Create_Database();
+                    break;
+
+            }
+        }
+    }
+}
